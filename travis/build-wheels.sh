62f4f4af45092149f5e40299bd7ce0cc505ee859
Olivier Belanger <belangeo@gmail.com>
2019-04-03 17:22:28 -0400

    Trying to figure it out!

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,7 +4,7 @@ set -e -x
 # Install a system package required by our library
 yum install -y portaudio-devel portmidi-devel libsndfile-devel liblo-devel
 
-# ...
+echo "test /usr/include"
 
 # Compile wheels
 cd /io/pyo/
