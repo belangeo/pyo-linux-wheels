b3a10846cd1fad0df11ca90317eb6806d92ae4e4
Olivier Belanger <belangeo@gmail.com>
2019-04-03 12:12:15 -0400

    Trying to figure it out!

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -5,15 +5,16 @@ set -e -x
 yum install -y portaudio-devel portmidi-devel libsndfile-devel liblo-devel
 
 # Compile wheels
+cd /io/pyo/
 for PYBIN in /opt/python/*/bin; do
     #"${PYBIN}/pip" install -r /io/dev-requirements.txt
-    "${PYBIN}/python" /io/pyo/setup.py build_ext --use-double --use-jack
-    "${PYBIN}/pip" wheel /io/pyo/ -w wheelhouse/
+    "${PYBIN}/python" setup.py build_ext --use-double --use-jack
+    "${PYBIN}/pip" wheel . -w wheelhouse/
 done
 
 # Bundle external shared libraries into the wheels
 for whl in wheelhouse/*.whl; do
-    auditwheel repair "$whl" -w /io/pyo/wheelhouse/
+    auditwheel repair "$whl" -w wheelhouse/
 done
 
 # Install packages and test
