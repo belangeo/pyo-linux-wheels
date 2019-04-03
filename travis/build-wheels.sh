bc17800c508ebcc5a386ad01acb9d22c46abd8cf
Olivier Belanger <belangeo@gmail.com>
2019-04-03 12:04:23 -0400

    Trying to figure it out!

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -4,7 +4,7 @@ set -e -x
 # Install a system package required by our library
 yum install -y portaudio-devel portmidi-devel libsndfile-devel liblo-devel
 
-ls -al
+ls -al io/
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
