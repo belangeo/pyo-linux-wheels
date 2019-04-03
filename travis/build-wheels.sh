0614a508dc42d081a8c550f6b7aa464e03d00380
Olivier Belanger <belangeo@gmail.com>
2019-04-03 12:16:49 -0400

    Trying to figure it out!

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -8,7 +8,7 @@ yum install -y portaudio-devel portmidi-devel libsndfile-devel liblo-devel
 cd /io/pyo/
 for PYBIN in /opt/python/*/bin; do
     #"${PYBIN}/pip" install -r /io/dev-requirements.txt
-    "${PYBIN}/python" setup.py build_ext --use-double --use-jack
+    "${PYBIN}/python" setup.py build_ext --minimal --use-double --use-jack
     "${PYBIN}/pip" wheel . -w wheelhouse/
 done
 
