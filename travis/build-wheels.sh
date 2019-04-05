43845bd1c90e19381479a2f90971eab327a4cbd1
Olivier Belanger <belangeo@gmail.com>
2019-04-05 08:48:33 -0400

    Try to configure libsndfile build from sources.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -3,14 +3,16 @@ set -e -x
 
 # Install a system package required by our library
 yum update
-yum install -y portaudio-devel portmidi-devel libsndfile-devel liblo-devel
+yum install -y autoconf autogen 
+#portaudio-devel portmidi-devel libsndfile-devel liblo-devel
 
-#cd /io/deps/
-#tar -xzf libsndfile-1.0.28.tar.gz
-#cd libsndfile-1.0.28
-#./autogen.sh
-#./configure --prefix=/usr
-#sudo make make install
+cd /io/deps/
+tar -xzf libsndfile-1.0.28.tar.gz
+cd libsndfile-1.0.28
+./autogen.sh
+./configure --prefix=/usr
+# make
+#sudo make install
 
 # Compile wheels
 cd /io/pyo/
