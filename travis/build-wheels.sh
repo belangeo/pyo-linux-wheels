e69020117b1806eb1f9b38540a0b8695b296fbf1
Olivier Belanger <belangeo@gmail.com>
2019-04-09 09:28:06 -0400

    Try to build alsa-lib.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -65,6 +65,15 @@ make install
 ldconfig
 cd ..
 
+echo ====== Build and install alsa-lib. ======
+tar -xjf alsa-lib-1.1.8.tar.bz2
+cd alsa-lib-1.1.8
+./configure --prefix=/usr
+make
+make install
+ldconfig
+cd ..
+
 echo ====== Build and install jack-audio-connection-kit. ======
 tar -xzf jack-audio-connection-kit-0.125.0.tar.gz
 cd jack-audio-connection-kit-0.125.0
