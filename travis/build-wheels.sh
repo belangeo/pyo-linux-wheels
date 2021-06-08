9c08d3b3b2b8fb12949debccfabf987a9806d8aa
Olivier Belanger <belangeo@gmail.com>
2021-06-07 21:28:05 -0400

    Revert back to manylinux1.

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -136,7 +136,7 @@ cd /io/pyo/
 
 rm -rf wheeltmp
 
-VERSIONS="cp36-cp36m cp37-cp37m cp38-cp38"
+VERSIONS="cp36-cp36m cp37-cp37m cp38-cp38 cp39-cp39"
 
 for version in $VERSIONS; do
     if [[ -d /opt/python/${version} ]]; then
